f00d2caf560fb74afaf498f07e56f3ce26acb523
Ching-yu Lin <ching.yulin@cognite.com>
2022-03-26 16:54:36 +0100

    add docstring for plot method

--- src/hjerter/visualization/2d.py
+++ src/hjerter/visualization/2d.py
@@ -1,12 +1,29 @@
+from typing import Optional
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 from hjerter.algorithms.k_mean import k_mean
 
 
-def plot(data: np.ndarray, clusters: np.ndarray) -> None:
+def plot(
+    data: np.ndarray,
+    dim0: int = 0,
+    dim1: int = 1,
+    clusters: Optional[np.ndarray] = None,
+) -> None:
+    """Plot two dimensions of data in `data`, optionally colored with `clusters`
+
+    Args:
+        data (np.ndarray): (N, D), N is # of data points,
+                         D is the feature dimension
+        dim0 (int, optional): dim0-th column of data to be plot on x-axis. Defaults to 0.
+        dim1 (int, optional): dim1-th column of data to be plot on y-axis. Defaults to 1.
+        clusters (Optional[np.ndarray], optional): (N, 1), the value is the clusters
+                        where the data belong to. Defaults to None.
+    """
     fig, ax = plt.subplots()
-    ax.scatter(x=data[:, 0], y=data[:, 1], c=clusters)
+    ax.scatter(x=data[:, dim0], y=data[:, dim1], c=clusters)
     plt.show()
 
 
@@ -15,4 +32,4 @@ if __name__ == "__main__":
     D = 2
     data = np.random.normal(10, size=(N, D))
     clusters = k_mean(data=data, k=100, eps=0.01)
-    plot(data, clusters)
+    plot(data, clusters=clusters)
